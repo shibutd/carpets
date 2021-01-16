0bae8ff41542589b3fa044e22ebd486d00b416b2
unknown <shibut_d@mail.ru>
2021-01-16 18:38:50 +0500

    added contact page

---
+++ frontend/pages/contact.js
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+import Layout from '../components/Layout'
+
+
+export default function Contact() {
+  const [textAreaValue, setTextAreaValue] = useState('')
+  const [contactValue, setContactValue] = useState('')
+
+  const validateText = (text) => text.replace(/ /g, '')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (validateText(textAreaValue) && validateText(contactValue)) {
+      alert(`Сообщение отправлено: ${textAreaValue}`)
+      setTextAreaValue('')
+      setContactValue('')
+    }
+  }
+
+  return (
+    <Layout
+      title={"Обратная связь | Алладин96.ру"}
+    >
+      <section className="contact">
+        <h1>Обратная связь</h1>
+        <h5>Здесь Вы можете задать вопрос, оставить жалобу или внести предложение:</h5>
+        <div className="contact-wrapper">
+          <form className="contact-form" onSubmit={handleSubmit}>
+            <textarea
+              rows="10"
+              cols="30"
+              value={textAreaValue}
+              onChange={(e) => setTextAreaValue(e.target.value)}
+            />
+            <label htmlFor="text">
+              Телефон или Email для связи:
+            </label>
+            <input
+              name="text"
+              type="text"
+              value={contactValue}
+              onChange={(e) => setContactValue(e.target.value)}
+            />
+            <button onClick={handleSubmit}>Отправить</ button>
+          </form>
+        </div>
+      </section>
+    </Layout>
+  )
+}
